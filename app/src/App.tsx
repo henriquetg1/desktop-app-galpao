859ca93897d868f5e156674d8cfbb3156acb9bb2
henriquetg1 <henriqueturco1@gmail.com>
2024-07-07 19:52:07 -0300

    feat: Edição e exclusão de setores pronta

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CriacaoGalpao from './pages/CriacaoGalpao';
+import CriacaoSetor from './pages/CriacaoSetor';
 import EdicaoGalpao from './pages/EdicaoGalpao';
+import EdicaoSetor from './pages/EdicaoSetor';
+import Galpao from './pages/Galpao'
 import './App.css';
 
 function App() {
@@ -11,6 +14,10 @@ function App() {
       <Route path="/galpoes" element={<Home />} />
       <Route path="/galpoes/criar" element={<CriacaoGalpao />} />
       <Route path="/galpoes/editar/:id" element={<EdicaoGalpao />} />
+      <Route path="/galpoes/:id" element={<Galpao />} />
+      <Route path="/galpoes/:id/setores/novo" element={<CriacaoSetor />} />
+      <Route path="/galpoes/:id/setores/editar/:idSetor" element={<EdicaoSetor />} />
+
 
     </Routes>
   );
---
+++ app/src/pages/EdicaoSetor.tsx
@@ -0,0 +1,154 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Box, Button, Container, TextField, Typography, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+import { Setor, updateSetor, deleteSetor } from '../services/setorService';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/system';
+
+// Tema personalizado
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Arial, sans-serif'
+  },
+  palette: {
+    primary: {
+      main: '#006437', // Verde do Palmeiras
+    },
+    secondary: {
+      main: '#1D78AC', // Azul mais claro
+    },
+    warning: {
+      main: '#C40F0F', // Vermelho
+    },
+    info: {
+      main: '#000000', // Preto
+    },
+  }
+});
+
+const EdicaoSetor: React.FC = () => {
+    const [nome, setNome] = useState<string>('');
+    const [open, setOpen] = useState(false);
+    const navigate = useNavigate();
+    const { idSetor } = useParams<{ idSetor: string }>(); // Usar o id do setor na URL
+    const { id } = useParams(); // Usar o id do galpão na URL
+
+    useEffect(() => {
+        loadSetor();
+    }, []);
+
+    async function loadSetor() {
+        fetch(`http://localhost:8080/setores/${idSetor}`, { // Usar idSetor aqui
+            method: 'GET',
+        }).then(response => {
+            return response.json();
+        }).then((data: Setor) => {
+            setNome(data.nome);
+        });
+    }
+
+    const handleSave = async () => {
+        await updateSetor(idSetor, { nome }); // Usar idSetor aqui
+        navigate(`/galpoes/${id}`); // Redirecionar para a lista de galpões
+    };
+
+    const handleDelete = async () => {
+        await deleteSetor(idSetor); // Usar idSetor aqui
+        navigate(`/galpoes/${id}`); // Redirecionar para a lista de galpões
+    }
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container>
+                <Typography
+                    sx={{ display: 'block', fontWeight: 'bold', fontSize: 25, lineHeight: 2 }}
+                    color="black"
+                    variant="h2"
+                    gutterBottom
+                >
+                    Edição de Setor
+                </Typography>
+                <TextField
+                    label="Nome"
+                    variant="outlined"
+                    fullWidth
+                    value={nome}
+                    onChange={(e) => setNome(e.target.value)}
+                    style={{ marginBottom: 20 }}
+                />
+                <Box style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleSave}
+                        style={{ margin: 'auto', marginRight: '3%', width: '150px', padding: '10px', fontSize: '14px' }}
+                    >
+                        Salvar
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        color="info"
+                        onClick={() => navigate(`/galpoes/${id}`)}
+                        style={{ margin: 'auto', marginLeft: '3%', width: '150px', padding: '10px', fontSize: '14px' }}
+                    >
+                        Cancelar
+                    </Button>
+                </Box>
+                <Typography
+                    sx={{ display: 'block', fontWeight: 'bold', fontSize: 20, lineHeight: 1, marginTop: 5}}
+                    color="black"
+                    variant="h4"
+                    gutterBottom
+                >
+                    Deseja excluir este setor?
+                </Typography>
+                <Button
+                    variant="contained"
+                    color="warning"
+                    onClick={handleClickOpen}
+                    style={{ marginTop: 20, width: '150px', padding: '10px', fontSize: '14px' }}
+                >
+                    Excluir
+                </Button>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">{"Confirmação de Exclusão"}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            Tem certeza que deseja excluir este setor?
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose} color="info">
+                            Cancelar
+                        </Button>
+                        <Button
+                            onClick={() => {
+                                handleDelete();
+                                handleClose();
+                            }}
+                            color="warning"
+                            autoFocus
+                        >
+                            Excluir
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </Container>
+        </ThemeProvider>
+    );
+}
+
+export default EdicaoSetor;
