ecedddb25ba50a25e0cda664c2bf271ba7fc88b1
henriquetg1 <henriqueturco1@gmail.com>
2024-07-07 19:52:28 -0300

    style: Design da página de criação de galpão padronizado

--- app/src/pages/CriacaoGalpao.tsx
+++ app/src/pages/CriacaoGalpao.tsx
@@ -1,10 +1,30 @@
-
-// Função para criar um novo galpão (nome e endereço) tsx
-
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button, Container, TextField } from '@mui/material';
+import { Button, Container, TextField, Typography, Box } from '@mui/material';
 import { createGalpao } from '../services/galpaoService';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/system';
+
+// Tema personalizado
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Arial, sans-serif'
+  },
+  palette: {
+    primary: {
+      main: '#006437', // Verde do Palmeiras
+    },
+    secondary: {
+      main: '#1D78AC', // Azul mais claro
+    },
+    warning: {
+      main: '#C40F0F', // Vermelho
+    },
+    info: {
+      main: '#000000', // Preto
+    },
+  }
+});
 
 const CriacaoGalpao: React.FC = () => {
   const [nome, setNome] = useState<string>('');
@@ -17,8 +37,16 @@ const CriacaoGalpao: React.FC = () => {
   };
     
 return (
+    <ThemeProvider theme={theme}>
     <Container>
-    <h1>Criar Galpão</h1>
+    <Typography
+    sx={{ display: 'block', fontWeight: 'bold', fontSize: 25, lineHeight: 2 }}
+    color="black"
+    variant="h2"
+    gutterBottom
+    >
+    Criação de Galpão
+    </Typography>
     <TextField
         label="Nome"
         variant="outlined"
@@ -35,22 +63,26 @@ return (
         onChange={(e) => setEndereco(e.target.value)}
         style={{ marginBottom: 20 }}
     />
-    <Button
-        variant="contained"
-        color="primary"
-        onClick={handleSave}
-    >
-        Salvar
-    </Button>
-    <Button
-        variant="contained"
-        color="secondary"
-        onClick={() => navigate('/galpoes')}
-        style={{ marginLeft: 10 }}
-    >
-        Cancelar
-    </Button>
+    <Box style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+        <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSave}
+            style={{ margin: 'auto', marginRight: '3%', width: '150px', padding: '10px', fontSize: '14px' }}
+        >
+            Salvar
+        </Button>
+        <Button
+            variant="outlined"
+            color="info"
+            onClick={() => navigate('/galpoes')}
+            style={{ margin: 'auto', marginLeft: '3%', width: '150px', padding: '10px', fontSize: '14px' }}
+        >
+            Cancelar
+        </Button>
+    </Box>
     </Container>
+    </ThemeProvider>
     );
 }
 
