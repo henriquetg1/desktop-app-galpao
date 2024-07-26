522c13494af2ce5abfbedbfac3b864cf465acad3
henriquetg1 <henriqueturco1@gmail.com>
2024-07-26 11:29:32 -0300

    feat: Botão para página de todos os itens

--- app/src/pages/Galpao.tsx
+++ app/src/pages/Galpao.tsx
@@ -1,3 +1,5 @@
+// GalpaoPage.tsx
+
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Box, Button, Container, Typography, createTheme, IconButton, CircularProgress } from '@mui/material';
@@ -6,7 +8,6 @@ import { ThemeProvider } from '@mui/system';
 import { Galpao, getGalpao } from '../services/galpaoService';
 import { Setor } from '../services/setorService';
 import EditIcon from '@mui/icons-material/Edit';
-
 
 // Tema personalizado
 const theme = createTheme({
@@ -40,120 +41,141 @@ const containerVariants = {
 };
 
 const GalpaoPage = () => {
-    const [galpao, setGalpao] = useState<Galpao | null>(null);
-    const [setores, setSetores] = useState<Setor[]>([]);
-    const [loading, setLoading] = useState(true); // Estado para gerenciar o carregamento
-    const navigate = useNavigate();
-    const { id } = useParams();
+  const [galpao, setGalpao] = useState<Galpao | null>(null);
+  const [setores, setSetores] = useState<Setor[]>([]);
+  const [loading, setLoading] = useState(true); // Estado para gerenciar o carregamento
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-    useEffect(() => {
-        setLoading(true);
-        getGalpao(id!)
-            .then((data) => {
-                setGalpao(data);
-                setSetores(data.setores || []);
-                setLoading(false);
-            })
-            .catch(() => navigate('/404'));
-            setLoading(false);
-    }, [id, navigate]);
+  useEffect(() => {
+    setLoading(true);
+    getGalpao(id!)
+      .then((data) => {
+        setGalpao(data);
+        setSetores(data.setores || []);
+        setLoading(false);
+      })
+      .catch(() => navigate('/404'));
+  }, [id, navigate]);
 
-    const handleSetorClick = (setorId) => {
-        navigate(`/setores/${setorId}`);
-    };
+  const handleSetorClick = (setorId: string) => {
+    navigate(`/setores/${setorId}`);
+  };
 
-    const handleEditSetorClick = (setorId, e) => {
-        e.stopPropagation();
-        navigate(`/galpoes/${id}/setores/editar/${setorId}`);
-    };
+  const handleEditSetorClick = (setorId: string, e: React.MouseEvent) => {
+    e.stopPropagation();
+    navigate(`/galpoes/${id}/setores/editar/${setorId}`);
+  };
 
-    const handleNavigateNovoSetor = () => {
-        navigate(`/galpoes/${id}/setores/novo`);
-    };
+  const handleNavigateNovoSetor = () => {
+    navigate(`/galpoes/${id}/setores/novo`);
+  };
 
-    return (
-        <ThemeProvider theme={theme}>
-          <Container component={motion.div} initial="hidden" animate="visible" variants={containerVariants} sx={{ textAlign: 'center' }}>
-            <Typography
-              sx={{ display: 'block', fontWeight: 'bold', fontSize: 25, lineHeight: 2 }}
-              color="black"
-              variant="h2"
-              gutterBottom
-            >
-              Galpão: {galpao?.nome}
-            </Typography>
-            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-              <Typography
-                sx={{ display: 'block', fontWeight: 'bold', fontSize: 20, lineHeight: 1, marginTop: 5, marginBottom: 2}}
-                color="black"
-                variant="h4"
-                gutterBottom
-              >
-                Acesse os setores cadastrados abaixo:
-              </Typography>
-              {loading ? (
-                <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-                  <CircularProgress />
-                </Box>
-              ) : setores.length > 0 ? (
-                setores.map((setor) => (
-                  <Box key={setor.id} sx={{ marginBottom: 2, display: 'flex', alignItems: 'center' }}>
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      fullWidth
-                      onClick={() => handleSetorClick(setor.id)}
-                      sx={{ display: 'flex', justifyContent: 'space-between', padding: 2 }}
+  const handleNavigateTodosItens = () => {
+    navigate(`/galpoes/${id}/itens`); // Navegar para a página de todos os itens do galpão
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component={motion.div} initial="hidden" animate="visible" variants={containerVariants} sx={{ textAlign: 'center' }}>
+        <Typography
+          sx={{ display: 'block', fontWeight: 'bold', fontSize: 25, lineHeight: 2 }}
+          color="black"
+          variant="h2"
+          gutterBottom
+        >
+          Galpão: {galpao?.nome}
+        </Typography>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+          <Typography
+            sx={{ display: 'block', fontWeight: 'bold', fontSize: 20, lineHeight: 1, marginTop: 5, marginBottom: 2 }}
+            color="black"
+            variant="h4"
+            gutterBottom
+          >
+            Veja todos os itens do galpão:
+          </Typography>
+          <Button
+            variant="outlined"
+            color="secondary"
+            onClick={handleNavigateTodosItens} // Alterado para navegar para a página de itens
+            sx={{ display: 'block', width: '100%', padding: 2, fontWeight: 'bold', fontSize: 16, border: '2px solid #1D78AC', '&:hover': {
+              borderWidth: '3px', // Ensure the border thickness remains the same on hover
+            }, }}
+          >
+            Itens
+          </Button>
+          <Typography
+            sx={{ display: 'block', fontWeight: 'bold', fontSize: 20, lineHeight: 1, marginTop: 5, marginBottom: 2 }}
+            color="black"
+            variant="h4"
+            gutterBottom
+          >
+            Acesse os setores cadastrados abaixo:
+          </Typography>
+          {loading ? (
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+              <CircularProgress />
+            </Box>
+          ) : setores.length > 0 ? (
+            setores.map((setor) => (
+              <Box key={setor.id} sx={{ marginBottom: 2, display: 'flex', alignItems: 'center' }}>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  fullWidth
+                  onClick={() => handleSetorClick(setor.id)}
+                  sx={{ display: 'flex', justifyContent: 'space-between', padding: 2 }}  
+                >
+                  <Box sx={{ textAlign: 'left' }}>
+                    <Typography
+                      sx={{ display: 'block', fontWeight: 'bold', fontSize: 16, lineHeight: 1, marginBottom: 2, marginTop: 1.5 }}
+                      color="inherit"
+                      variant="h6"
+                      gutterBottom
                     >
-                      <Box sx={{ textAlign: 'left' }}>
-                        <Typography
-                          sx={{ display: 'block', fontWeight: 'bold', fontSize: 16, lineHeight: 1, marginBottom: 2, marginTop: 1.5}}
-                          color="inherit"
-                          variant="h6"
-                          gutterBottom
-                        >
-                          {setor.nome || 'Nome do setor não disponível'}
-                        </Typography>
-                      </Box>
-                      <IconButton
-                        aria-label="edit"
-                        color='inherit'
-                        onClick={(e) => {
-                          e.stopPropagation(); // Impede o clique no ícone de navegar para a página de detalhes
-                          handleEditSetorClick(setor.id, e);
-                        }}
-                      >
-                        <EditIcon />
-                      </IconButton>
-                    </Button>
+                      {setor.nome || 'Nome do setor não disponível'}
+                    </Typography>
                   </Box>
-                ))
-              ) : (
-                <Typography>Nenhum setor disponível</Typography>
-              )}
-            </Box>
-            <br />
-            <Box style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={handleNavigateNovoSetor}
-                style={{ margin: 'auto', marginRight: '3%', width: '150px', padding: '10px', fontSize: '14px' }}
-              >
-                Novo Setor
-              </Button>
-              <Button
-                variant="outlined"
-                color="info"
-                onClick={() => navigate('/galpoes')}
-                style={{ margin: 'auto', marginLeft: '3%', width: '150px', padding: '10px', fontSize: '14px' }}
-              >
-                Cancelar
-              </Button>
-            </Box>
-          </Container>
-        </ThemeProvider>
-    );
+                  <IconButton
+                    aria-label="edit"
+                    color='inherit'
+                    onClick={(e) => {
+                      e.stopPropagation(); // Impede o clique no ícone de navegar para a página de detalhes
+                      handleEditSetorClick(setor.id, e);
+                    }}
+                  >
+                    <EditIcon />
+                  </IconButton>
+                </Button>
+              </Box>
+            ))
+          ) : (
+            <Typography>Nenhum setor disponível</Typography>
+          )}
+        </Box>
+        <br />
+        <Box style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleNavigateNovoSetor}
+            style={{ margin: 'auto', marginRight: '3%', width: '150px', padding: '10px', fontSize: '14px' }}
+          >
+            Novo Setor
+          </Button>
+          <Button
+            variant="outlined"
+            color="info"
+            onClick={() => navigate('/galpoes')}
+            style={{ margin: 'auto', marginLeft: '3%', width: '150px', padding: '10px', fontSize: '14px' }}
+          >
+            Cancelar
+          </Button>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default GalpaoPage;
